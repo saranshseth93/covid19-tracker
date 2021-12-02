717f16a0aed22a82f8734ee9a8bc4d08d081aba5
Saransh Seth <saranshseth93@gmail.com>
2021-12-02 21:16:24 +0530

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -26,6 +26,8 @@ export const fetchData = async(country) => {
 export const fetchDailyData = async() => {
     try {
         const {data} = await axios.get(`${url}/daily`);
+        
+        console.log(data);
 
         const modifiedData = data.map((dailyData) => ({
             confirmed: dailyData.confirmed.total,
